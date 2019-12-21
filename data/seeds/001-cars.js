7bbee698f2bd79ad68c6dd7b109effc36917a18c
Dennis Padiernos <depadiernos@gmail.com>
2019-12-20 22:36:22 -0600

    update seed with all required fields

--- data/seeds/001-cars.js
+++ data/seeds/001-cars.js
@@ -1,9 +1,9 @@
 exports.seed = async function(knex) {
   await knex("cars").truncate()
   await knex("cars").insert([
-    { vin: "", make: "Toyota", model: "Yaris", mileage: "100000" },
-    { vin: "", make: "Hyundai", model: "Elantra", mileage: "35000" },
-    { vin: "", make: "Ford", model: "Escape", mileage: "45000" },
-    { vin: "", make: "Honda", model: "Accord", mileage: "90000" }
+    { vin: "3US2UKAX01AUT6JSR", make: "Toyota", model: "Yaris", mileage: "100000" },
+    { vin: "JHN409FLBNJ5BLYLP", make: "Hyundai", model: "Elantra", mileage: "35000" },
+    { vin: "CWER07QQH08EXCNY1", make: "Ford", model: "Escape", mileage: "45000" },
+    { vin: "NWW5YZ5YIW31Z7FNX", make: "Honda", model: "Accord", mileage: "90000" }
   ])
 }
